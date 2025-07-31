23634443d2bf10cd6debcd572669336a9158de07
Davide Forcina <forcina@grupporetina.com>
2025-07-31 12:28:19 +0200

    plugins e configurazioni iniziali

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'macplast' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         'm,p-t2aL5fW}p0`Kvb*k{gbFO`o1}| !r1DE7Ii+z:RrsHe%6ODm.*,[v,k?E55@');
+define('SECURE_AUTH_KEY',  ':8;Aun/HW0[?:GU-2b./dY?A:/cI T-Eu_)6s q0#]0|k,MW<7q8}FI)z4DL,p$@');
+define('LOGGED_IN_KEY',    'vc2eM.+!cq$_S&k-Lu+xpTX1UjlA*I&z 0-hNJ}zxN~T&[nRBZS(;b_iYuAYkkan');
+define('NONCE_KEY',        'Zz5qp-5r4~pBr(keFuM},,x|QJne:T|z|;f+{,mT1]2KDwvxKsxd6+Ydj;,:kV?X');
+define('AUTH_SALT',        'fyH^3)wqvvYis1<~5w:Q_=E WY#vZQZ*-#*/gZ]!%/n8@Rs.2DlIG6pQD8J|n8iG');
+define('SECURE_AUTH_SALT', 'UkWTqS:y#ER_Y/ [w2y?^[}tJfHj&vf#x>:/ o,-Vl*,|NJ2S;UOh!!<@_ta!^Rm');
+define('LOGGED_IN_SALT',   'Mx?kEj(?U+Ba9;DKS_;b7KuDIoz(6-G-;-~Z:S+P@p+.s(4(p[uNx63Wj*HGM_h@');
+define('NONCE_SALT',       'M]KaM7-L.V%+?=H8s]SB?3V&og8@M3x@[CPj3$d{.<aK=[|KtO{W+*Gr+u>+Ju.D');
 
 /**#@-*/
 
---
+++ wp/wp-content/languages/wpml/wordpress-es_ES.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['messages'=>['URL product_cat tax slug'=>'categoria-producto','URL product_tag tax slug'=>'etiqueta-producto','URL slug: product'=>'producto','product'=>'producto','product-category'=>'categoria-producto','product-tag'=>'etiqueta-producto']];
